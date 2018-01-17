72aae33fcc3b6bc7c7eafa9e4e698a38cdbd2210
lichao3140 <396229938@qq.com>
2018-01-17 19:51:24 +0800

    Initial commit

---
+++ .d
@@ -0,0 +1,4 @@
+/home/cocoa/soft/linphone-android/gen/com/android/vending/billing/IInAppBillingService.java : \
+  /home/cocoa/soft/linphone-android/src/com/android/vending/billing/IInAppBillingService.aidl
+
+/home/cocoa/soft/linphone-android/src/com/android/vending/billing/IInAppBillingService.aidl :
---
+++ src/com/quhwa/MyApplication.java
@@ -0,0 +1,15 @@
+package com.quhwa;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by Administrator on 2018-01-17.
+ */
+
+public class MyApplication extends MultiDexApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
