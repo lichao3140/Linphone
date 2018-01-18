1407325efcaaaddc357a84c54a020997585d81e0
lichao3140 <396229938@qq.com>
2018-01-18 22:25:54 +0800

    fix

--- src/com/quhwa/MyApplication.java
+++ src/com/quhwa/MyApplication.java
@@ -1,15 +1,31 @@
 package com.quhwa;
 
-import android.support.multidex.MultiDexApplication;
+import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
 
 /**
  * Created by Administrator on 2018-01-17.
  */
 
-public class MyApplication extends MultiDexApplication {
+public class MyApplication extends Application {
+    public static MyApplication application;
+
+    public static MyApplication getInstance() {
+        return application;
+    }
 
     @Override
     public void onCreate() {
         super.onCreate();
+        application = this;
     }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        // 将MultiDex注入到项目中
+        MultiDex.install(this);
+    }
+
 }
